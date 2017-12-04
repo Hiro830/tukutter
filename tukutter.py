dc7ef7a7e8136c4537c44e5780f980b174241e56
Hiro <shimoji1122@gmail.com>
2017-12-04 15:59:02 +0000

    ログイン&Top機能実装(途中)_user登録&つぶやき追加

--- tukutter.py
+++ tukutter.py
@@ -2,9 +2,64 @@ import MySQLdb
 from flask import Flask, render_template,request,redirect
 
 application = Flask(__name__)
+
 @application.route('/')
 def index():
     return redirect('http://localhost:8080/static/login.html')
+
+@application.route('/new')
+def show_new():
+    #新規追加画面を表示する
+    return render_template('new_account.html')
+
+@application.route('/new', methods=['POST'])
+def donew():
+
+    login_n = request.form['new_login_ID']  #login_id
+    pass_n = request.form['new_Pass']       #password
+    re_pass_n = request.form['new_Re_Pass']
+    name_n = request.form['new_name']       #username
+#####入力がなかった場合はもう一度入力してくださいってでるようにしたい#####
+    if pass_n == re_pass_n:  #照合に合格した時の処理
+      #mysqlに接続する
+      db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter', charset='utf8')
+      con = db.cursor()
+      #取得したタスクの内容をtodoテーブルに追加する
+      sql = 'insert into users(Login,name,pass) value (%s,%s,%s)'
+      con.execute(sql,[login_n,name_n,pass_n])
+      db.commit()
+      #DBの切断
+      db.close()
+      con.close()
+
+      html = redirect('http://localhost:8080/')
+    else:
+      html = render_template('fail_account.html')
+    return html
+
+
+@application.route('/guchi')
+def guchiru():
+    #新規追加画面を表示する
+    return render_template('tubuyaita-.html')
+
+@application.route('/guchi', methods=['POST'])
+def tubuyaita():
+    tubuyaki = request.form['guchita']  #login_id
+    #mysqlに接続する
+    db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter', charset='utf8')
+    con = db.cursor()
+
+    #取得したつぶやきをtweetInfoに登録する。　がしかし、user_idがわからなくなってまった。。ひっぱりかたどしよ
+
+    #sql = 'insert into tweetInfo(users_id,tweet,time_now) value (%s,%s,%s)'
+    #con.execute(sql,[,tubuyaki,'CURRENT_TIMESTAMP'])
+    #db.commit()
+    #DBの切断
+    db.close()
+    con.close()
+
+    return tubuyaki
 
 @application.route('/login', methods=['POST'])
 def login_suru():
@@ -27,7 +82,6 @@ def login_suru():
     #passの照合を行う
     if userinfo[0][3] == Pass_G:  #照合に合格した時の処理
       list=[]   #ツイートを格納する配列
-      #sql = 'SELECT tweet,delete_flg FROM tweetInfo WHERE users_id = %s'
       sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE users_id = %s'
       con.execute(sql,[test_id])
       test1 = con.fetchall()    #userのツイート
@@ -41,22 +95,20 @@ def login_suru():
       test2 = result    #userがフォローしている人達のid
 
       for row in test2: #フォローしている人のツイートを取得
-          #sql = 'SELECT tweet,delete_flg FROM tweetInfo WHERE users_id = %s'
-          sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE users_id = %s'
+          sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE users_id = %s'# order by tweetInfo.id desc
           #print(row[0])
           con.execute(sql,[row[0]]) #各ユーザのツイート
           test3 = con.fetchall()    #各ユーザのツイート
           for row in test3: #ツイート情報を個別に配列する
-              print (row[0])    #users_id
-              print(row[2])     #users_name
-              print(row[5])     #tweet_id
-              print(row[7])     #tweet
-              print(row[8])     #tweet_time
-
+              #print (row[0])    #users_id
+              #print(row[2])     #users_name
+              #print(row[5])     #tweet_id
+              #print(row[7])     #tweet
+              #print(row[8])     #tweet_time
               list.append(row)
 
-##topに反映できるようにhtmlも変更必要##
-      html = render_template('top.html', name=test_name,row=list)
+      ##listにソートかけれたらgood
+      html = render_template('top.html', name=test_name,rows=list)
 
     else:   #照合に失敗した場合の処理を記入する
       print('間違ってます')
