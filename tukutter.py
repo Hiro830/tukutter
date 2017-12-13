6aa77d4dcaa99c498b114a4a74e3c3a45fd32eee
Hiro <shimoji1122@gmail.com>
2017-12-13 11:10:39 +0000

    第一弾,,

--- tukutter.py
+++ tukutter.py
@@ -11,6 +11,55 @@ def index1():
 def index2():
     return render_template('login.html')
 
+#@application.route('/plofile/<username>')
+#def plofile(username=None):
+
+@application.route('/follow/<username>')
+def follow(username=None):
+    data = request.cookies.get('username', None)
+    if data is None:
+      html = render_template('login.html')
+    else:
+      db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter_2', charset='utf8')
+      con = db.cursor()
+      sql = 'SELECT * FROM users WHERE name = %s'
+      con.execute(sql,[data])
+      userinfo =con.fetchone()  #user_idを取得
+
+      sql = 'SELECT * FROM follow WHERE follow_me = %s'
+      con.execute(sql,[userinfo[0]])
+      follow = con.fetchall()
+      print(follow)
+      if follow is ():   #値がないときって()でいいのか？？
+        html = redirect('http://localhost:8080/top')
+        sql = 'insert into follow(follow_me,follow_you) value (%s,%s)'
+        con.execute(sql,[userinfo[0],username])
+        db.commit()
+      else:
+        for row in follow:
+            if row[2] == username:
+              hantei = 10
+              #delete
+              ##sql = 'DELETE FROM follow WHERE id = %s'
+              ##con.execute(sql,[row[0]])
+              print(hantei)
+            else:
+              #insert
+              ##sql = 'insert into follow(follow_me,follow_you) value (%s,%s)'
+              ##con.execute(sql,[userinfo[0],username])
+              ##db.commit()
+              hantei = 20
+              print(hantei)
+
+      print(username)
+
+      db.close()
+      con.close()
+      html = redirect('http://localhost:8080/top')
+
+    return html
+
+
 @application.route('/top')
 def index3():
     #requestオブジェクトからCookieを取得する
@@ -48,6 +97,25 @@ def index3():
       con.close()
     return html
 
+@application.route('/favoritelog')
+def favarite_suru():
+    #########tweetInfo.idが欲しい###########
+    data = request.cookies.get('username', None)
+    if data is None:
+      html = render_template('login.html')
+    else:
+      db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter_2', charset='utf8')
+      con = db.cursor()
+
+      sql = 'SELECT * FROM users WHERE name = %s'
+      con.execute(sql,[data])
+      userinfo =con.fetchone()  #user_idを取得
+
+
+      db.close()
+      con.close()
+
+    return data
 
 @application.route('/favorite')
 def favarite():
@@ -61,35 +129,37 @@ def favarite():
       sql = 'SELECT * FROM users WHERE name = %s'
       con.execute(sql,[data])
       userinfo =con.fetchone()  #user_idを取得
-      print (userinfo)
+      #print (userinfo)
 
       sql = 'select * from favorite where user_id = %s'
       con.execute(sql,[userinfo[0]])
       favarite_info = con.fetchall()
       print(favarite_info)
-      print(favarite_info[0][2])   #tweet_id
-      list=[]   #お気に入りのツイートを格納する配列
-      for row in favarite_info: #favariteのtweetを1つずつ処理する
-          print(row)
-          sql = 'select * from tweetInfo where id = %s'
-          sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE tweetInfo.id = %s'
-          con.execute(sql,[row[2]])
-          tweet_info = con.fetchall()
-          print(tweet_info)
-          list.append(tweet_info[0])
-          print(list[0][2]) #users_name
-          print(list[0][9]) #tweet
-          print(list[0][10])    #tweet_time
-      html = render_template('favorites.html', user=userinfo,favarites=list)
+      if favarite_info is ():   #値がないときって()でいいのか？？
+        html = redirect('http://localhost:8080/top')
+      else:
+        list=[]   #お気に入りのツイートを格納する配列
+        for row in favarite_info: #favariteのtweetを1つずつ処理する
+            print(row)
+            sql = 'select * from tweetInfo where id = %s'
+            sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE tweetInfo.id = %s'
+            con.execute(sql,[row[2]])
+            tweet_info = con.fetchall()
+            print(tweet_info)
+            list.append(tweet_info[0])
+            print(list[0][2]) #users_name
+            print(list[0][9]) #tweet
+            print(list[0][10])    #tweet_time
+            #html = render_template('index.html', user=userinfo,favarites=list)
+            html = render_template('index.html', id=userinfo[0],name=data,rows=list)
       #DBの切断
       db.close()
       con.close()
       print(data)
-      print (list)
-      print (list[1][2])
+      #print (list)
+      #print (list[1][2])
 
     return html
-
 
 @application.route('/plofile/<username>')
 def plofile(username=None):
@@ -109,7 +179,10 @@ def plofile(username=None):
       con.execute(sql,[username])
       userinfo =con.fetchone()
       print (userinfo)
-
+      if data == userinfo[2]:
+        hantei = 1
+      else:
+        hantei = 0
       #user自身のツイートを取得
       #sql = 'select * from tweetInfo where users_id = %s'
       sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id WHERE users_id = %s'
@@ -123,8 +196,14 @@ def plofile(username=None):
       print(tweet_list[0][2])   #name
       print(tweet_list[0][9])   #tweet
       print(tweet_list[0][10])  #tweet_time
+      print (hantei)
 
-      html = render_template('profile.html', user=user,tweets=tweet_list)
+      db.close()
+      con.close()
+      html = render_template('profile.html', user=user,tweets=tweet_list,hantei = hantei)
+    print(data)
+    print(userinfo[2])
+    print (hantei)
     return html
 
 @application.route('/logout')
@@ -134,6 +213,69 @@ def logout_suru():
     Value = 'None'
     html.set_cookie('username', Value,expires)
     return html
+
+
+
+@application.route('/search')
+def search():
+    data = request.cookies.get('username', None)
+    if data is None:
+      html = render_template('login.html')
+    else:
+      db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter_2', charset='utf8')
+      con = db.cursor()
+      sql = 'SELECT * from users inner join tweetInfo on tweetInfo.users_id = users.id'
+      con.execute(sql)
+      tweet_info = con.fetchall()
+      list=[]
+      for row in tweet_info:
+          list.append(row)
+
+      db.close()  #DBの切断
+      con.close()
+      html = render_template('search.html',name=data,rows=list)
+    return html
+
+@application.route('/searchget',methods=['get'])
+def search_get():
+    data = request.cookies.get('username', None)
+    if data is None:
+      html = render_template('login.html')
+    else:
+      db = MySQLdb.connect( user='root', passwd='A12qwerzxcv123', host='localhost', db='tukutter_2', charset='utf8')
+      con = db.cursor()
+      serch_word = request.args.get('search_query')
+      print (serch_word)
+      sql = 'SELECT * from tweetInfo where tweet like %s'
+      con.execute(sql,[serch_word])
+      tweet_info = con.fetchall()
+      print(tweet_info)
+
+      db.close()  #DBの切断
+      con.close()
+      html = render_template('search.html')
+    return html
+
+@application.route('/profile')
+def profile_edit():
+    return render_template('profile_edit.html')
+
+@application.route('/profile', methods=['POST'])
+def profile_write():
+    pic = request.form['file']
+    userid = request.form['login_ID']
+    n_pass = request.form['Pass']       #password
+    re_pass = request.form['Re_Pass']
+    name = request.form['username']       #username
+    intro = request.form['intro']
+
+    print(pic)
+    print(userid)
+    print(n_pass)
+    print(re_pass)
+    print(name)
+    print(intro)
+    return intro
 
 @application.route('/new')
 def show_new():
